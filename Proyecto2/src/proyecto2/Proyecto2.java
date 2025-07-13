9af812022a38eb5a8fbba89f34a5541ef5866247
Ricardo Ruiz <ricardo.ruizc@uned.cr>
2025-07-12 19:48:20 -0600

    Create main GUI with buttons and actionlisteners

--- Proyecto2/src/proyecto2/Proyecto2.java
+++ Proyecto2/src/proyecto2/Proyecto2.java
@@ -8,7 +8,52 @@ package proyecto2;
  *
  * @author Ricardo
  */
-public class Proyecto2 {
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class Proyecto2 extends JFrame{
+    
+    public Proyecto2() {
+        // configuración del frame principal para el programa.
+        // tomando de referencia mi Proyecto1 y adaptado para el presente proyecto
+        setTitle("Gestión de Tienda Departamental y de Artículos");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(5, 1, 5, 5));
+        setSize(600,500);
+        setLocationRelativeTo(null);
+        
+        
+        // botones del menu principal 
+        
+        /*
+        btnRegistroDpto
+        btnRegistroArt
+        btnEliminarArt
+        btnTrasladoArt
+        btnEliminarDpto        
+        
+        */        
+        
+        JButton btnRegistro = new JButton("Registro de Departamentos");
+        JButton btnRegistroArt = new JButton("Registro de Artículos");
+        JButton btnEliminarArt = new JButton("Eliminación de artículos");
+        JButton btnTrasladoArt = new JButton("Traslado de artículos");
+        JButton btnEliminarDpto = new JButton("Eliminación de departamentos");
+        
+        // asignacion de ActionListener a cada boton para abrir la ventana respectiva
+        .addActionListener(e -> new ().setVisible(true));
+        .addActionListener(e -> new ().setVisible(true));
+        .addActionListener(e -> new ().setVisible(true));
+        .addActionListener(e -> new ().setVisible(true));
+        .addActionListener(e -> new ().setVisible(true));
+        
+    }
+    
+    
+    
 
     /**
      * @param args the command line arguments
