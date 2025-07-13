9f3b554c9d56c5626dd0a6bfa11228b0942421ae
Ricardo Ruiz <ricardo.ruizc@uned.cr>
2025-07-13 14:20:26 -0600

    finish LIFO behavior for Departamento class array and articulo array FIFO behavior

--- Proyecto2/src/proyecto2/Departamento.java
+++ Proyecto2/src/proyecto2/Departamento.java
@@ -13,8 +13,32 @@ public class Departamento {
     private String nombre;
 
     private static final int MAX_ARTICULOS = 20;
-   
+    private Articulo[] articulos = new Articulo[MAX_ARTICULOS];
+    private int frente = 0;
+    private int fin = 0;
+    
+    public Departamento(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+    
+    public boolean agregarArticulo(Articulo art) {
+        if ((fin + 1) % MAX_ARTICULOS == frente) {
+            return false; // cola llena 
+        }
+        articulos[fin] = art;
+        fin = (fin + 1) % MAX_ARTICULOS;
+        return true;
+    }
 
+    public Articulo[] getArticulos() {
+        Articulo[] resultado = new Articulo[(fin - frente + MAX_ARTICULOS) % MAX_ARTICULOS];
+        int idx = 0;
+        for (int i = frente; i != fin; i = (i + 1) % MAX_ARTICULOS) {
+            resultado[idx++] = articulos[i];
+        }
+        return resultado;
+    }
     
     
     public int getId() {
