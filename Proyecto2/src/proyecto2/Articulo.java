a626e21b12f172c6ce6b998a0d9530ab394c5f4d
Ricardo Ruiz <ricardo.ruizc@uned.cr>
2025-07-13 14:16:45 -0600

    set Aritculo class variables

--- Proyecto2/src/proyecto2/Articulo.java
+++ Proyecto2/src/proyecto2/Articulo.java
@@ -9,5 +9,25 @@ package proyecto2;
  * @author Ricardo
  */
 public class Articulo {
-    
+    private int id;
+    private String nombre;
+    private String categoria;
+
+    public Articulo(int id, String nombre, String categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
 }
