8ea08f32dfbb1671708e02535c2c646d0a48456a
Ricardo Ruiz <ricardo.ruizc@uned.cr>
2025-07-13 14:59:47 -0600

    prototype methods

--- Proyecto2/src/proyecto2/RegistroArticulos.java
+++ Proyecto2/src/proyecto2/RegistroArticulos.java
@@ -26,13 +26,45 @@ public class RegistroArticulos extends JFrame {
     private int topeDeptos;
     
     
-    public RegistroArticulos() {
+    public RegistroArticulos(Departamento[] pilaDeptos, int topeDeptos) {
+        this.pilaDeptos = pilaDeptos;
+        this.topeDeptos = topeDeptos;
+        
         setTitle("Registro de Artículos");
         setSize(900,600);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
         
+        modeloDepartamentos = new DefaultTableModel(new Object[]{"ID", "Nombre"}, 0);
+        tablaDepartamentos = new JTable(modeloDepartamentos);
+        tablaDepartamentos.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        JScrollPane scrollTabla = new JScrollPane(tablaDepartamentos);
+        add(scrollTabla, BorderLayout.CENTER);
+        
+        JPanel panelArticulo = new JPanel();
+        panelArticulo.setLayout(new GridLayout(3, 2, 10, 10));
+        panelArticulo.setBorder(BorderFactory.createTitledBorder("Agregar nuevo artículo"));
+
+        panelArticulo.add(new JLabel("Nombre del Artículo:"));
+        txtNombreArticulo = new JTextField();
+        panelArticulo.add(txtNombreArticulo);
+        
+        JButton btnAgregar = new JButton("Agregar Artículo");
+        btnAgregar.addActionListener(e -> registrarArticulo());
+        panelArticulo.add(btnAgregar);
+
+        add(panelArticulo, BorderLayout.SOUTH);
+        
+        llenarTablaDepartamentos();
+    }
+    
+    private void llenarTablaDepartamentos(){
+        
+    }
+    
+    private void registrarArticulo(){
+
     }
     
 }
